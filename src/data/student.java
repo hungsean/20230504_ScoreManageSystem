31aba43f70357b88bd13037793c180a21d8fc075
hungsean <hungsean0419@gmail.com>
2023-05-05 14:35:38 +0800

    add updateSum, updateAvg, and fix updateSD

--- src/data/student.java
+++ src/data/student.java
@@ -5,51 +5,74 @@ import java.util.*;
 public class student {
 
     Map<Integer, Score> studentMap = new HashMap<Integer, Score>();
+    
+    float chineseSum = 0;
+    float mathSum = 0;
+    float chineseAvg = 0;
+    float mathAvg = 0;
     float chineseSD = 0;
     float mathSD = 0;
     
     public void addStudent(int studentNumber, Score score) 
     {
         studentMap.put(studentNumber, score);
-        updateSD();
+        update();
     }
 
     public void removeStudent(int studentNumber) 
     {
         studentMap.remove(studentNumber);
-        updateSD();
+        update();
     }
 
     public void updateStudent(int studentNumber, Score score) 
     {
         studentMap.put(studentNumber, score);
+        update();
+    }
+
+    public void update()
+    {
+        updateSum();
+        updateAvg();
         updateSD();
+
+    }
+
+    public void updateSum()
+    {
+        float chineseSum = 0;
+        float mathSum = 0;
+        for (Map.Entry<Integer, Score> entry : studentMap.entrySet()) 
+        {
+            Score score = entry.getValue();
+            chineseSum += score.getChineseScore();
+            mathSum += score.getMathScore();
+        }
+        this.chineseSum = chineseSum;
+        this.mathSum = mathSum;
+    }
+
+    public void updateAvg()
+    {
+        float chineseAvg = chineseSum / studentMap.size();
+        float mathAvg = mathSum / studentMap.size();
+        this.chineseAvg = chineseAvg;
+        this.mathAvg = mathAvg;
     }
 
     public void updateSD()
     {
-        float chineseSum = 0;
-        float mathSum = 0;
-        float chineseAvg = 0;
-        float mathAvg = 0;
         float chineseSD = 0;
         float mathSD = 0;
-        int count = 0;
-        for (Map.Entry<Integer, Score> entry : studentMap.entrySet()) {
-            Score score = entry.getValue();
-            chineseSum += score.getChineseScore();
-            mathSum += score.getMathScore();
-            count++;
-        }
-        chineseAvg = chineseSum / count;
-        mathAvg = mathSum / count;
-        for (Map.Entry<Integer, Score> entry : studentMap.entrySet()) {
+        for (Map.Entry<Integer, Score> entry : studentMap.entrySet()) 
+        {
             Score score = entry.getValue();
             chineseSD += Math.pow(score.getChineseScore() - chineseAvg, 2);
             mathSD += Math.pow(score.getMathScore() - mathAvg, 2);
         }
-        chineseSD = (float) Math.sqrt(chineseSD / count);
-        mathSD = (float) Math.sqrt(mathSD / count);
+        chineseSD = (float) Math.sqrt(chineseSD / studentMap.size());
+        mathSD = (float) Math.sqrt(mathSD / studentMap.size());
         this.chineseSD = chineseSD;
         this.mathSD = mathSD;
     }
